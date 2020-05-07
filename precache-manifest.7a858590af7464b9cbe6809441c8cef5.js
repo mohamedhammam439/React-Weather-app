72ac388fec74791dddebb22f25bdd76e41425773
mohamedhammam439 <mohamedhammam439@gmail.com>
2020-05-07 07:32:02 +0200

    Updates

---
+++ precache-manifest.7a858590af7464b9cbe6809441c8cef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda7f6f54d10ed81b51783cac08fe090",
+    "url": "/React-Weather-app/index.html"
+  },
+  {
+    "revision": "82798ee7b9b73453052b",
+    "url": "/React-Weather-app/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "614cc66ee41233f2e21f",
+    "url": "/React-Weather-app/static/css/main.8ea03942.chunk.css"
+  },
+  {
+    "revision": "82798ee7b9b73453052b",
+    "url": "/React-Weather-app/static/js/2.23e2f723.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Weather-app/static/js/2.23e2f723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614cc66ee41233f2e21f",
+    "url": "/React-Weather-app/static/js/main.9760875f.chunk.js"
+  },
+  {
+    "revision": "d57c4a6fe5ec2358636b",
+    "url": "/React-Weather-app/static/js/runtime-main.285e16c0.js"
+  }
+]);
